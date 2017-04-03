de9180b59199ea389ee3a525735f36af58c6462b
cantei <gcantei@gmail.com>
2017-04-03 09:53:31 +0700

    ยาที่ รพ.สต.ใช้อยู่ในปัจจุบัน

---
+++ drug_24digit_active_use.sql
@@ -0,0 +1,16 @@
+
+################################################################################
+# JHCIS
+################################################################################
+SELECT d.drugcode
+,c.drugname,c.drugflag 
+,n.item,n.tradename,n.comp 
+FROM visitdrug d
+LEFT  JOIN cdrug c
+ON d.drugcode=c.drugcode 
+LEFT JOIN drug_thailand n
+ON c.drugcode24=n.id 
+WHERE c.drugflag='1'
+GROUP BY d.drugcode
+ORDER BY c.drugname 
+################################################################################
