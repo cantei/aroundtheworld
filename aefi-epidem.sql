8bc16178cb35198b2d9859163cfef4ef85bab54c
cantei <gcantei@gmail.com>
2017-04-23 10:52:40 +0700

    Update aefi-epidem.sql

--- aefi-epidem.sql
+++ aefi-epidem.sql
@@ -1,6 +1,8 @@
 SELECT p.CID,m.HN
+,m.DISEASE,m.`NAME`,m.SEX,concat(m.AGEY,'-',m.AGEM,'-',m.AGED) as AGE
+,m.ADDRESS
 ,v.villagecode,v.villagename,v.tamboncode
-,m.DISEASE,m.`NAME`,m.sex,m.AGEY,m.AGEM,m.AGED,m.ADDRESS,DATE_FORMAT(m.DATESICK,'%Y-%m-%d') as DATESICK,DATE_FORMAT(m.DATERECORD,'%Y-%m-%d') as DATERECORD 
+,DATE_FORMAT(m.DATESICK,'%Y-%m-%d') as DATESICK,DATE_FORMAT(m.DATERECORD,'%Y-%m-%d') as DATERECORD 
 FROM me_epidem m  /* temp  TABLE  */
 LEFT JOIN person p
 ON m.HN=p.HN 
