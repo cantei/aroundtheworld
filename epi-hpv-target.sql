e25d0ad556b42ecefcfd442676219d00181a2e01
cantei <gcantei@gmail.com>
2017-04-27 13:38:16 +0700

    จำนวนนักเรียนหญิง ป๕  โดย  พงษ์เทพ  สร้อยอินทร์

--- epi-hpv-target.sql
+++ epi-hpv-target.sql
@@ -1,14 +1,19 @@
-SELECT personstudent.pid,person.idcard,CONCAT(ctitle.titlename,person.fname," ",person.lname) as fullname
--- ,convert(concat(getAgeYearNum(birth,curdate())," ปี ",getAgeMonthNum(birth,curdate())," เดือน ",getAgeDayNum(birth,curdate())," วัน ")using utf8)as age 
-,getageymd(person.birth,CURRENT_DATE()) AS age
-,house.hno,right(house.villcode,2)as mu,cschoolclass.classname,villageschool.schoolname#,studenthealthcheck.result,LAST_DAY(studenthealthcheck.dateservice)
-FROM personstudent
-INNER JOIN person on personstudent.pcucodeperson = person.pcucodeperson and personstudent.pid=person.pid
-INNER JOIN ctitle on person.prename=ctitle.titlecode
-INNER JOIN house on person.pcucodeperson=house.pcucodeperson and person.hcode=house.hcode
-INNER JOIN cschoolclass on personstudent.classeducate=cschoolclass.classcode
-INNER JOIN villageschool on personstudent.schoolno=villageschool.schoolno
-#LEFT JOIN studenthealthcheck ON personstudent.pid=studenthealthcheck.pid AND personstudent.pcucodeperson=studenthealthcheck.pcucodeperson
-WHERE
-personstudent.classeducate = '7' AND person.sex='2' AND personstudent.yeareducate='2557'
-ORDER BY villageschool.schoolname ASC
+SELECT t.schoolname,count(*) n 
+FROM 
+(
+SELECT 
+p.pcucodeperson,p.idcard 
+,p.pid ,CONCAT(ctitle.titlename,p.fname,' ',p.lname)as pname 
+,TIMESTAMPDIFF(year,date_format(p.birth,'%Y-%m-%d'),date_format(now(),'%Y-%m-%d')) age 
+,villageschool.schoolname ,cschoolclass.classname 
+FROM person p 
+INNER JOIN personstudent ON personstudent.pcucodeperson = p.pcucodeperson AND personstudent.pid = p.pid 
+LEFT JOIN villageschool ON villageschool.pcucode = personstudent.pcucode AND villageschool.villcode = personstudent.villcode AND villageschool.schoolno = personstudent.schoolno 
+INNER JOIN cschoolclass ON cschoolclass.classcode = personstudent.classeducate 
+LEFT JOIN ctitle ON ctitle.titlecode=p.prename
+WHERE personstudent.classeducate ='7' AND personstudent.yeareducate='2559' AND p.sex='2'
+AND RIGHT(villageschool.villcode,2) <>'00' 
+AND CONCAT(p.pid,p.pcucodeperson) NOT IN (SELECT CONCAT(persondeath.pid,persondeath.pcucodeperson) FROM persondeath ) 
+ORDER BY villageschool.schoolname ,cschoolclass.classname ASC
+) as t
+GROUP BY t.schoolname
