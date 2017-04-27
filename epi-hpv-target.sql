bc116910cb8953524c260df3cd2c2a57f5b72217
cantei <gcantei@gmail.com>
2017-04-27 15:49:43 +0700

    จำนวนนักเรียนหญิง ป.๕  เป้าหมาย HPV

--- epi-hpv-target.sql
+++ epi-hpv-target.sql
@@ -1,21 +1,24 @@
 # จำนวนนักเรียนหญิง ป.๔ ปีการศึกษา ๒๕๕๙ # 
 
-SELECT t.schoolname,count(*) n 
+SELECT t.villcode,t.schoolname,count(*) as n
 FROM 
 (
-SELECT 
-p.pcucodeperson,p.idcard 
-,p.pid ,CONCAT(ctitle.titlename,p.fname,' ',p.lname)as pname 
-,TIMESTAMPDIFF(year,date_format(p.birth,'%Y-%m-%d'),date_format(now(),'%Y-%m-%d')) age 
-,villageschool.schoolname ,cschoolclass.classname 
-FROM person p 
-INNER JOIN personstudent ON personstudent.pcucodeperson = p.pcucodeperson AND personstudent.pid = p.pid 
-LEFT JOIN villageschool ON villageschool.pcucode = personstudent.pcucode AND villageschool.villcode = personstudent.villcode AND villageschool.schoolno = personstudent.schoolno 
-INNER JOIN cschoolclass ON cschoolclass.classcode = personstudent.classeducate 
-LEFT JOIN ctitle ON ctitle.titlecode=p.prename
-WHERE personstudent.classeducate ='7' AND personstudent.yeareducate='2559' AND p.sex='2'
-AND RIGHT(villageschool.villcode,2) <>'00' 
-AND CONCAT(p.pid,p.pcucodeperson) NOT IN (SELECT CONCAT(persondeath.pid,persondeath.pcucodeperson) FROM persondeath ) 
-ORDER BY villageschool.schoolname ,cschoolclass.classname ASC
+SELECT v.villcode,v.schoolname
+,p.yeareducate
+,p.pcucodeperson,p.pid
+,m.fname,m.lname,m.birth,m.sex
+,c.classname 
+FROM  personstudent p 
+LEFT JOIN villageschool  v 
+ON v.pcucode = p.pcucode AND v.villcode = p.villcode AND v.schoolno = p.schoolno 
+INNER JOIN cschoolclass c
+ON c.classcode = p.classeducate
+LEFT JOIN person m 
+ON p.pcucodeperson=m.pcucodeperson AND p.pid=m.pid  
+WHERE
+-- v.villcode='67010207' AND 
+p.classeducate='7' AND 
+m.sex='2'
+ORDER BY c.classcode,m.pid
 ) as t
-GROUP BY t.schoolname
+GROUP BY t.villcode
