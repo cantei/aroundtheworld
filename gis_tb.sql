a6b99a6089abe086ba075528fc578e833ab4085c
cantei <gcantei@gmail.com>
2017-05-30 07:34:13 +0700

    Update gis_tb.sql

--- gis_tb.sql
+++ gis_tb.sql
@@ -1,3 +1,47 @@
+# 
+
+SELECT
+	p.pcucodeperson,
+		p.pid,
+		p.fname,
+		CONCAT(c.titlename,p.fname,' ',p.lname) AS pname,
+		p.birth,
+		h.hno,
+		h.villcode,
+		h.xgis,
+		h.ygis,
+visit.pid as pid1,
+visit.visitno,
+count(distinct visit.visitno) as count_visit,
+max(visit.visitdate) as visitdate,
+visithomehealthindividual.homehealthtype,
+max(chomehealthtype.homehealthmeaning) as homehealthmeaning,
+max(visithomehealthindividual.patientsign) as patientsign,
+max(visithomehealthindividual.homehealthdetail) as homehealthdetail,
+max(visithomehealthindividual.homehealthresult) as homehealthresult,
+max(visithomehealthindividual.homehealthplan) as homehealthplan,
+max(visithomehealthindividual.dateappoint) as dateappoint,
+concat(ctitle.titlename,`user`.fname,`user`.lname) as userh,
+max(visithomehealthindividual.`user`) as `user`
+FROM visit
+LEFT JOIN person p
+ON p.pcucodeperson=visit.pcucode AND p.pid=visit.pid 
+		Inner Join house h ON p.pcucodeperson = h.pcucode AND p.hcode = h.hcode
+		Inner Join village v ON h.pcucode = v.pcucode AND h.villcode = v.villcode
+		left Join ctitle c ON p.prename = c.titlecode
+Inner Join visithomehealthindividual ON visit.pcucode = visithomehealthindividual.pcucode AND visit.visitno = visithomehealthindividual.visitno
+Inner Join chomehealthtype ON visithomehealthindividual.homehealthtype = chomehealthtype.homehealthcode
+INNER JOIN `user` ON visit.pcucodeperson = `user`.pcucode AND visithomehealthindividual.`user` = `user`.username
+left JOIN ctitle ON `user`.prename = ctitle.titlecode
+where visit.visitdate between '2017-01-01' and '2017-10-01' 
+and (visit.flagservice <'04' OR visit.flagservice is null OR length(trim(visit.flagservice))=0 )
+AND visithomehealthindividual.homehealthtype='1A014'
+group by visit.pcucodeperson,visit.pid;
+
+
+
+# before update 
+
 SELECT *
 FROM 
 (
