4f3e6b76e8bf6e69c8783303acde1e784a519eb4
cantei <gcantei@gmail.com>
2018-01-29 14:49:03 +0700

    กลุ่มโรคเรื้อรัง รายคน

--- all_chronic.sql
+++ all_chronic.sql
@@ -31,6 +31,7 @@
 			`hiv` date default NULL,
 			`cirrhosis` date default NULL,
 			`chepatitis` date default NULL,
+			`osteoarthritis` date default NULL,
 			`crheumatoid` date default NULL,
 			`crenalfailure` date default NULL
 		) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -65,7 +66,7 @@ UPDATE tmp_chronic_all  t0
 INNER JOIN
 (
 	SELECT p.pcucodeperson,p.pid
-	,SPLIT_STR(GROUP_CONCAT(c.datefirstdiag ORDER BY c.datefirstdiag SEPARATOR ','),',',1) as datediag 
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag 
 	FROM  person p
 	LEFT JOIN personchronic c
 	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
@@ -81,7 +82,7 @@ UPDATE tmp_chronic_all  t0
 INNER JOIN
 (
 	SELECT p.pcucodeperson,p.pid
-	,SPLIT_STR(GROUP_CONCAT(c.datefirstdiag ORDER BY c.datefirstdiag SEPARATOR ','),',',1) as datediag 
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag 
 	FROM  person p
 	LEFT JOIN personchronic c
 	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
@@ -97,7 +98,7 @@ UPDATE tmp_chronic_all  t0
 INNER JOIN
 (
 	SELECT p.pcucodeperson,p.pid
-	,SPLIT_STR(GROUP_CONCAT(c.datefirstdiag ORDER BY c.datefirstdiag SEPARATOR ','),',',1) as datediag 
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag  
 	FROM  person p
 	LEFT JOIN personchronic c
 	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
@@ -113,7 +114,7 @@ UPDATE tmp_chronic_all  t0
 INNER JOIN
 (
 	SELECT p.pcucodeperson,p.pid
-	,SPLIT_STR(GROUP_CONCAT(c.datefirstdiag ORDER BY c.datefirstdiag SEPARATOR ','),',',1) as datediag 
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag  
 	FROM  person p
 	LEFT JOIN personchronic c
 	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
@@ -123,13 +124,12 @@ INNER JOIN
 ON t0.pcucodeperson=t1.pcucodeperson AND t0.pid=t1.pid 
 SET t0.stroke=t1.datediag;
 
-
 # add ihd
 UPDATE tmp_chronic_all  t0
 INNER JOIN
 (
 	SELECT p.pcucodeperson,p.pid
-	,SPLIT_STR(GROUP_CONCAT(c.datefirstdiag ORDER BY c.datefirstdiag SEPARATOR ','),',',1) as datediag 
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag 
 	FROM  person p
 	LEFT JOIN personchronic c
 	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
@@ -139,13 +139,12 @@ INNER JOIN
 ON t0.pcucodeperson=t1.pcucodeperson AND t0.pid=t1.pid 
 SET t0.ihd=t1.datediag;
 
-
 # add copd
 UPDATE tmp_chronic_all  t0
 INNER JOIN
 (
 	SELECT p.pcucodeperson,p.pid
-	,SPLIT_STR(GROUP_CONCAT(c.datefirstdiag ORDER BY c.datefirstdiag SEPARATOR ','),',',1) as datediag 
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag 
 	FROM  person p
 	LEFT JOIN personchronic c
 	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
@@ -155,40 +154,169 @@ INNER JOIN
 ON t0.pcucodeperson=t1.pcucodeperson AND t0.pid=t1.pid 
 SET t0.copd=t1.datediag;
 
-# add copd
+# add Asthma 
 UPDATE tmp_chronic_all  t0
 INNER JOIN
 (
 	SELECT p.pcucodeperson,p.pid
-	,SPLIT_STR(GROUP_CONCAT(c.datefirstdiag ORDER BY c.datefirstdiag SEPARATOR ','),',',1) as datediag 
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag 
 	FROM  person p
 	LEFT JOIN personchronic c
 	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
-	WHERE substr(REPLACE(chroniccode,'.',''),1,4) BETWEEN 'J449' AND 'J449' 
+	WHERE substr(REPLACE(chroniccode,'.',''),1,3) BETWEEN 'J45' AND 'J46' 
 	GROUP BY p.pcucodeperson,p.pid
 ) as t1
 ON t0.pcucodeperson=t1.pcucodeperson AND t0.pid=t1.pid 
-SET t0.copd=t1.datediag;
+SET t0.asthma=t1.datediag;
 
 
 
+# add Emphysema        J43
+UPDATE tmp_chronic_all  t0
+INNER JOIN
+(
+	SELECT p.pcucodeperson,p.pid
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag 
+	FROM  person p
+	LEFT JOIN personchronic c
+	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
+	WHERE substr(REPLACE(chroniccode,'.',''),1,3) BETWEEN 'J43' AND 'J43' 
+	GROUP BY p.pcucodeperson,p.pid
+) as t1
+ON t0.pcucodeperson=t1.pcucodeperson AND t0.pid=t1.pid 
+SET t0.emphysema=t1.datediag;
+
+
+# add Obesity              E66
+UPDATE tmp_chronic_all  t0
+INNER JOIN
+(
+	SELECT p.pcucodeperson,p.pid
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag 
+	FROM  person p
+	LEFT JOIN personchronic c
+	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
+	WHERE substr(REPLACE(chroniccode,'.',''),1,3) BETWEEN 'E66' AND 'E66' 
+	GROUP BY p.pcucodeperson,p.pid
+) as t1
+ON t0.pcucodeperson=t1.pcucodeperson AND t0.pid=t1.pid 
+SET t0.obesity=t1.datediag;
+
+# add Cancer  C00 – C97
+UPDATE tmp_chronic_all  t0
+INNER JOIN
+(
+	SELECT p.pcucodeperson,p.pid
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag 
+	FROM  person p
+	LEFT JOIN personchronic c
+	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
+	WHERE substr(REPLACE(chroniccode,'.',''),1,3) BETWEEN 'C00' AND 'C97' 
+	GROUP BY p.pcucodeperson,p.pid
+) as t1
+ON t0.pcucodeperson=t1.pcucodeperson AND t0.pid=t1.pid 
+SET t0.cancer=t1.datediag;
 
 
 
+# add  HIV/AIDS           B20 – B24
+UPDATE tmp_chronic_all  t0
+INNER JOIN
+(
+	SELECT p.pcucodeperson,p.pid
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag 
+	FROM  person p
+	LEFT JOIN personchronic c
+	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
+	WHERE substr(REPLACE(chroniccode,'.',''),1,3) BETWEEN 'B20' AND 'B24' 
+	GROUP BY p.pcucodeperson,p.pid
+) as t1
+ON t0.pcucodeperson=t1.pcucodeperson AND t0.pid=t1.pid 
+SET t0.hiv=t1.datediag;
 
 
 
+# add   Cirrhosis of liver  K70.3 ,K71.7,K74
+UPDATE tmp_chronic_all  t0
+INNER JOIN
+(
+
+	SELECT p.pcucodeperson,p.pid
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag 
+	FROM  person p
+	LEFT JOIN personchronic c
+	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
+	WHERE substr(REPLACE(chroniccode,'.',''),1,4) in ('K703','K717') OR substr(REPLACE(chroniccode,'.',''),1,3)='K74'
+	GROUP BY p.pcucodeperson,p.pid
+) as t1
+ON t0.pcucodeperson=t1.pcucodeperson AND t0.pid=t1.pid 
+SET t0.cirrhosis=t1.datediag;
+
+# add   Chronic hepatitis K73
+UPDATE tmp_chronic_all  t0
+INNER JOIN
+(
+	SELECT p.pcucodeperson,p.pid
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag 
+	FROM  person p
+	LEFT JOIN personchronic c
+	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
+	WHERE substr(REPLACE(chroniccode,'.',''),1,3)='K73'
+	GROUP BY p.pcucodeperson,p.pid
+) as t1
+ON t0.pcucodeperson=t1.pcucodeperson AND t0.pid=t1.pid 
+SET t0.chepatitis=t1.datediag;
+
+# add    Chronic Renal failure  N18
+
+UPDATE tmp_chronic_all  t0
+INNER JOIN
+(
+	SELECT p.pcucodeperson,p.pid
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag 
+	FROM  person p
+	LEFT JOIN personchronic c
+	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
+	WHERE substr(REPLACE(chroniccode,'.',''),1,3)='N18'
+	GROUP BY p.pcucodeperson,p.pid
+) as t1
+ON t0.pcucodeperson=t1.pcucodeperson AND t0.pid=t1.pid 
+SET t0.crenalfailure =t1.datediag;
 
 
+# add    Osteoarthritis      M15-M19,M47
 
+UPDATE tmp_chronic_all  t0
+INNER JOIN
+(
+	SELECT p.pcucodeperson,p.pid
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag 
+	FROM  person p
+	LEFT JOIN personchronic c
+	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
+	WHERE (substr(REPLACE(chroniccode,'.',''),1,3) BETWEEN 'M15' AND 'M19')  OR (substr(REPLACE(chroniccode,'.',''),1,3)='K74')
+	GROUP BY p.pcucodeperson,p.pid
+) as t1
+ON t0.pcucodeperson=t1.pcucodeperson AND t0.pid=t1.pid 
+SET t0.osteoarthritis=t1.datediag;
 
+# add     Rheumatoid arthritis   M05 – M06
 
+UPDATE tmp_chronic_all  t0
+INNER JOIN
+(
+	SELECT p.pcucodeperson,p.pid
+	,SPLIT_STR(GROUP_CONCAT(CAST(c.datefirstdiag AS CHAR(10000) CHARACTER SET utf8)  ORDER BY  c.datefirstdiag DESC SEPARATOR ','),',',1) as datediag 
+	FROM  person p
+	LEFT JOIN personchronic c
+	ON p.pcucodeperson=c.pcucodeperson AND p.pid=c.pid 
+	WHERE (substr(REPLACE(chroniccode,'.',''),1,3) BETWEEN 'M105' AND 'M06')  
+	GROUP BY p.pcucodeperson,p.pid
+) as t1
+ON t0.pcucodeperson=t1.pcucodeperson AND t0.pid=t1.pid 
+SET t0.crheumatoid=t1.datediag;
 
-
-
-
-
-
-
-
-
+###### Report 
+SELECT *
+FROM tmp_chronic_all
+ORDER BY (mumoi*1),(SPLIT_STR(hnomoi,'/', 1)*1),(SPLIT_STR(hnomoi,'/',2)*1);
