afb3970c107014b8494bcb5a98fe1e4edc1685f1
cantei <gcantei@gmail.com>
2018-02-28 23:44:02 +0700

    Create student_detail_jhcis.sql

---
+++ student_detail_jhcis.sql
@@ -0,0 +1,25 @@
+SELECT 
+-- s.pcucodeperson,s.pcucode,s.pid
+p.idcard,p.fname,p.lname,p.birth,p.sex,p.typelive 
+,h.hno,substr(h.villcode,7,2) as moo
+,s.villcode
+,v.villname
+-- ,s.schoolno
+,s.classeducate
+-- ,s.classroom
+,cs.classname
+,concat('ร.ร.',vs.schoolname) as nameschool
+FROM personstudent s
+LEFT JOIN village v
+ON s.pcucode=v.pcucode AND s.villcode=v.villcode 
+LEFT JOIN villageschool vs
+ON s.pcucode=vs.pcucode AND s.villcode=vs.villcode  AND s.schoolno=vs.schoolno 
+LEFT JOIN cschoolclass cs 
+ON s.classeducate=cs.classcode 
+LEFT JOIN person p
+ON s.pcucodeperson=p.pcucodeperson AND s.pid=p.pid 
+LEFT JOIN house h
+ON p.pcucodeperson=h.pcucode AND p.hcode=h.hcode
+WHERE v.villno<>'0'
+AND s.villcode='84120702'
+AND s.classeducate=9
