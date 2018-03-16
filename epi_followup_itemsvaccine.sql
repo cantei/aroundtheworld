0f1e0f05db009a7946e9bdcf66f915d0cc2e7db7
cantei <gcantei@gmail.com>
2018-03-16 23:05:42 +0700

    vaccine where need (stored procedure loop)

--- epi_followup_itemsvaccine.sql
+++ epi_followup_itemsvaccine.sql
@@ -1,6 +1,6 @@
 BEGIN
-			DROP TABLE IF EXISTS table11;
-      CREATE TABLE table11 
+			DROP TABLE IF EXISTS me_vaccine_where_needs;
+      CREATE TABLE me_vaccine_where_needs 
       (
 				pcucodeperson varchar(5) default  NULL,
         pid int(15) default NULL,
@@ -12,7 +12,7 @@ BEGIN
       ) 
       ENGINE=InnoDB DEFAULT CHARSET=utf8;
  
-		INSERT INTO table11 (pcucodeperson,pid,birth,age_day,age_week,age_month) 
+		INSERT INTO me_vaccine_where_needs (pcucodeperson,pid,birth,age_day,age_week,age_month) 
 			SELECT  p.pcucodeperson,p.pid,p.birth
 			,TIMESTAMPDIFF(day,p.birth,CURDATE()) as age_day
 			,TIMESTAMPDIFF(WEEK,p.birth,CURDATE()) as age_week
@@ -32,7 +32,7 @@ BLOCK1: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
 	OPEN personcursor;
@@ -47,13 +47,13 @@ BLOCK1: begin
 										WHERE e.vaccinecode='BCG' AND  e.pid=c_pid  AND TIMESTAMPDIFF(day,p.birth,e.dateepi) > 0);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1 THEN /*  checkage */
 								LEAVE bcg;
 						END IF;
 								if n =0 AND c_age_day >= 0 THEN 
-									UPDATE table11 SET memo='BCG' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='BCG' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP bcg;
  
@@ -70,7 +70,7 @@ BLOCK2: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
 	OPEN personcursor;
@@ -83,15 +83,15 @@ BLOCK2: begin
 										WHERE e.vaccinecode in ('DHB1','DTP1') AND  e.pid=c_pid  AND TIMESTAMPDIFF(WEEK,p.birth,e.dateepi) > 8);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1  THEN 
 								LEAVE dhb1;
 						END IF;
 								if    n = 0 AND l > 1  AND c_age_month > 2 THEN 
-									UPDATE table11 SET memo=concat(memo,',DHB1') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',DHB1') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 2 THEN 
-									UPDATE table11 SET memo='DHB1' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='DHB1' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP dhb1;
  
@@ -108,7 +108,7 @@ BLOCK3: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
@@ -121,15 +121,15 @@ BLOCK3: begin
 										WHERE e.vaccinecode in ('OPV1') AND  pid=c_pid);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1  THEN 
 								LEAVE opv1;
 						END IF;
 								if    n = 0 AND l > 1 AND c_age_month > 2  THEN 
-									UPDATE table11 SET memo=concat(memo,',OPV1') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',OPV1') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 2  THEN 
-									UPDATE table11 SET memo='OPV1' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='OPV1' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP opv1;
  
@@ -145,7 +145,7 @@ BLOCK4: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
 	OPEN personcursor;
@@ -157,15 +157,15 @@ BLOCK4: begin
 										WHERE e.vaccinecode in ('DHB2','DTP2') AND  pid=c_pid);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1  THEN 
 								LEAVE dhb2;
 						END IF;
 								if    n = 0 AND l > 1 AND c_age_month > 4  THEN 
-									UPDATE table11 SET memo=concat(memo,',DHB2') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',DHB2') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 4 THEN 
-									UPDATE table11 SET memo='DHB2' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='DHB2' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP dhb2;
  
@@ -182,7 +182,7 @@ BLOCK5: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
 	OPEN personcursor;
@@ -194,15 +194,15 @@ BLOCK5: begin
 										WHERE e.vaccinecode in ('OPV2') AND  pid=c_pid);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1 THEN 
 								LEAVE opv2;
 						END IF;
 								if    n = 0 AND l > 1 AND c_age_month > 4 THEN 
-									UPDATE table11 SET memo=concat(memo,',OPV2') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',OPV2') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 4 THEN 
-									UPDATE table11 SET memo='OPV2' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='OPV2' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP opv2;
  
@@ -217,7 +217,7 @@ BLOCK6: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
 	OPEN personcursor;
@@ -228,15 +228,15 @@ BLOCK6: begin
 										WHERE e.vaccinecode in ('DHB3','DTP3') AND  pid=c_pid);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1 THEN 
 								LEAVE dhb3;
 						END IF;
 								if    n = 0 AND l > 1  AND c_age_month > 6 THEN 
-									UPDATE table11 SET memo=concat(memo,',DHB3') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',DHB3') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 6 THEN 
-									UPDATE table11 SET memo='DHB3' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='DHB3' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP dhb3;
  
@@ -253,7 +253,7 @@ BLOCK7: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
 	OPEN personcursor;
@@ -265,15 +265,15 @@ BLOCK7: begin
 										WHERE e.vaccinecode in ('OPV3') AND  pid=c_pid);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1 THEN 
 								LEAVE opv3;
 						END IF;
 								if    n = 0 AND l > 1 AND c_age_month > 6 THEN 
-									UPDATE table11 SET memo=concat(memo,',OPV3') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',OPV3') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 6 THEN 
-									UPDATE table11 SET memo='OPV3' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='OPV3' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP opv3;
  
@@ -289,7 +289,7 @@ BLOCK8: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
 	OPEN personcursor;
@@ -301,15 +301,15 @@ BLOCK8: begin
 										WHERE e.vaccinecode in ('IPV-P') AND  pid=c_pid);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1 THEN 
 								LEAVE ipv;
 						END IF;
 								if    n = 0 AND l > 1  AND c_age_month > 4 THEN 
-									UPDATE table11 SET memo=concat(memo,',IPV') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',IPV') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 4 THEN 
-									UPDATE table11 SET memo='IPV' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='IPV' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP ipv;
  
@@ -325,7 +325,7 @@ BLOCK9: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
 	OPEN personcursor;
@@ -341,15 +341,15 @@ BLOCK9: begin
 													));
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1 THEN 
 								LEAVE mmr;
 						END IF;
 								if    n = 0 AND l > 1 AND c_age_month > 9 THEN 
-									UPDATE table11 SET memo=concat(memo,',MMR1') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',MMR1') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 9 THEN 
-									UPDATE table11 SET memo='MMR1' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='MMR1' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP mmr;
  
@@ -365,7 +365,7 @@ BLOCK10: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
 	OPEN personcursor;
@@ -377,15 +377,15 @@ BLOCK10: begin
 										WHERE e.vaccinecode in ('J11','JE1') AND  pid=c_pid);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1 THEN 
 								LEAVE j11;
 						END IF;
 								if    n = 0 AND l > 1  AND c_age_month > 12 THEN 
-									UPDATE table11 SET memo=concat(memo,',LAJE1') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',LAJE1') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 12 THEN 
-									UPDATE table11 SET memo='LAJE1' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='LAJE1' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP j11;
  
@@ -401,7 +401,7 @@ BLOCK11: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
@@ -413,15 +413,15 @@ BLOCK11: begin
 										WHERE e.vaccinecode in ('DTP4') AND  pid=c_pid);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1 THEN 
 								LEAVE dtp4;
 						END IF;
 								if    n = 0 AND l > 1  AND c_age_month > 18 THEN 
-									UPDATE table11 SET memo=concat(memo,',DTP4') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',DTP4') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 18 THEN 
-									UPDATE table11 SET memo='DTP4' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='DTP4' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP dtp4;
  
@@ -438,7 +438,7 @@ BLOCK12: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
 	OPEN personcursor;
@@ -450,15 +450,15 @@ BLOCK12: begin
 										WHERE e.vaccinecode in ('OPV4') AND  pid=c_pid);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1 THEN 
 								LEAVE opv4;
 						END IF;
 								if    n = 0 AND l > 1 AND c_age_month > 18 THEN 
-									UPDATE table11 SET memo=concat(memo,',OPV4') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',OPV4') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 18 THEN 
-									UPDATE table11 SET memo='OPV4' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='OPV4' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP opv4;
  
@@ -473,7 +473,7 @@ BLOCK13: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
 	OPEN personcursor;
@@ -485,15 +485,15 @@ BLOCK13: begin
 										WHERE e.vaccinecode in ('MMR2') AND  pid=c_pid);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1 THEN 
 								LEAVE mmr2;
 						END IF;
 								if    n = 0 AND l > 1 AND c_age_month > 30 THEN 
-									UPDATE table11 SET memo=concat(memo,',MMR2') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',MMR2') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 30 THEN 
-									UPDATE table11 SET memo='MMR2' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='MMR2' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP mmr2;
  
@@ -508,7 +508,7 @@ BLOCK14: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
 	OPEN personcursor;
@@ -520,15 +520,15 @@ BLOCK14: begin
 										WHERE e.vaccinecode in ('J12','JE2','JE3') AND  pid=c_pid);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1 THEN 
 								LEAVE j12;
 						END IF;
 								if    n = 0 AND l > 1 AND c_age_month > 30  THEN 
-									UPDATE table11 SET memo=concat(memo,',LAJE2') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',LAJE2') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 30 THEN 
-									UPDATE table11 SET memo='LAJE2' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='LAJE2' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP j12;
  
@@ -543,7 +543,7 @@ BLOCK15: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
 	OPEN personcursor;
@@ -554,15 +554,15 @@ BLOCK15: begin
 										WHERE e.vaccinecode in ('DTP5') AND  pid=c_pid);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1  THEN 
 								LEAVE dtp5;
 						END IF;
 								if    n = 0 AND l > 1 AND c_age_month > 48 THEN 
-									UPDATE table11 SET memo=concat(memo,',DTP5') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',DTP5') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 48 THEN 
-									UPDATE table11 SET memo='DTP5' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='DTP5' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP dtp5;
  
@@ -578,7 +578,7 @@ BLOCK16: begin
 	DECLARE c_age_month INT  ;
 	DECLARE n INT  ;
 	DECLARE l INT  ;
-	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  table11 ;
+	DEClARE 	personcursor CURSOR FOR SELECT pid,age_day,age_week,age_month FROM  me_vaccine_where_needs ;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET c_finished = 1;
 
 	OPEN personcursor;
@@ -590,15 +590,15 @@ BLOCK16: begin
 										WHERE e.vaccinecode in ('OPV5') AND  pid=c_pid);
 
 								SET l=(SELECT LENGTH(memo)
-										FROM table11  
+										FROM me_vaccine_where_needs  
 										WHERE  pid=c_pid);
 						IF c_finished = 1  THEN 
 								LEAVE opv5;
 						END IF;
 								if    n = 0 AND l > 1 AND c_age_month > 48 THEN 
-									UPDATE table11 SET memo=concat(memo,',OPV5') WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo=concat(memo,',OPV5') WHERE  pid=c_pid;
 								ELSEIF n =0 AND (l<1  OR ISNULL(l)) AND c_age_month > 48 THEN 
-									UPDATE table11 SET memo='OPV5' WHERE  pid=c_pid;
+									UPDATE me_vaccine_where_needs SET memo='OPV5' WHERE  pid=c_pid;
 								END IF; 
 				END LOOP opv5;
  
@@ -616,7 +616,7 @@ SELECT p.pcucodeperson,p.pid,p.idcard
 ,h.hno,substr(h.villcode,7,2) as moo 
 ,i.memo
 ,concat(v.fname,'    ',v.lname) as 'volanteer'
-FROM table11 i
+FROM me_vaccine_where_needs i
 INNER JOIN person p
 ON i.pcucodeperson=p.pcucodeperson AND i.pid=p.pid  
 INNER JOIN ctitle c
