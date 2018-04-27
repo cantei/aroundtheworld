dea0d6005df0ab72fd726c247178e88a24fb070d
cantei <gcantei@gmail.com>
2018-04-27 21:04:24 +0700

    ใบตรวจโภชนาการ คัดกรองอาการไข้

--- 01_epi02_appoint_individual.sql
+++ 01_epi02_appoint_individual.sql
@@ -1,16 +1,30 @@
--- Appointment Individual  ON  Any Month
+# FOR STEP1 CHECK Temp,BW,heigth
 
 SET  @thisyear=YEAR(CURDATE()) ;
-SET  @thismonth=(MONTH(CURDATE())+0) ;
+SET  @thismonth=MONTH(CURDATE()) ;
 
-SELECT a.dateappoint,p.pid,p.idcard as cid,p.fname,p.lname,p.birth,p.typelive
-			,TIMESTAMPDIFF(MONTH,p.birth,CURDATE()) as age_month
-			,CONCAT(p.mother,'-',p.father) as parents
-			,GROUP_CONCAT(a.vaccinecode ORDER BY a.vaccinecode SEPARATOR ',') as vaccine_needs	
-FROM visitepiappoint a
-INNER JOIN person p
-ON p.pcucodeperson=a.pcucodeperson AND p.pid=a.pid
-WHERE year(a.dateappoint)=@thisyear AND month(a.dateappoint)=@thismonth
-GROUP BY p.idcard
--- HAVING vaccine_needs LIKE '%DTP%'
-ORDER BY p.pid
+SELECT h.villcode,p.pcucodeperson,p.pid
+,p.idcard
+,CONCAT(c.titlename,p.fname,'   ',p.lname) as fullname
+,p.birth
+,concat(DATE_FORMAT(p.birth,'%d-%m'),'-',year(p.birth)+543) as born
+,TIMESTAMPDIFF(MONTH,p.birth,CURDATE()) as  'agemonth'
+,p.mother as 'มารดา'
+,concat(h.hno,'  ','หมู่ที่','  ',substr(h.villcode,8,1))  as addess
+,concat(v.fname,'  ',v.lname) as 'volanteer'
+,a.dateappoint
+,GROUP_CONCAT(a.vaccinecode)  as need_vaccine
+FROM visitepiappoint a 
+LEFT JOIN person p 
+ON a.pcucodeperson=p.pcucodeperson AND a.pid=p.pid 
+LEFT JOIN ctitle c
+ON p.prename=c.titlecode
+LEFT JOIN house h 
+ON  p.pcucodeperson=h.pcucode AND p.hcode=h.hcode
+LEFT JOIN person v
+ON h.pcucodepersonvola=v.pcucodeperson and	h.pidvola=v.pid 
+WHERE YEAR(a.dateappoint)=@thisyear
+AND MONTH (a.dateappoint)=(@thismonth+1)
+GROUP BY p.pcucodeperson,p.pid
+HAVING need_vaccine like '%D%'
+ORDER BY h.villcode,(SPLIT_STR(hno,'/', 1)*1),(SPLIT_STR(hno,'/',2)*1)
