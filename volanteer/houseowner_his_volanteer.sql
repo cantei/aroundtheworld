dbaf44e0002329b8760f85b9ce09b966b4b93ba6
cantei <gcantei@gmail.com>
2018-12-08 09:49:57 +0700

    เจ้าบ้าน จำนวนผู้อยู่อาศัย และ อสม.ประจำบ้าน

--- volanteer/houseowner_his_volanteer.sql
+++ volanteer/houseowner_his_volanteer.sql
@@ -1,11 +1,15 @@
-SELECT t0.villcode,t0.hno
+SELECT t0.villcode
+,concat(' ',t0.hno) as hno
 ,concat(t0.fname,'   ',t0.lname) as houseowner
-,t1.hnomoi
-,concat(t1.provcodemoi,t1.distcodemoi,t1.subdistcodemoi,t1.mumoi) as areamoi
 ,sum(IF(t1.typelive in ('1','2'),1,0)) AS moi 
 ,sum(IF(t1.typelive in ('1','3'),1,0)) AS nlivea
+
+,GROUP_CONCAT(concat(' ',t1.hnomoi)) as hnomoi
+,concat(t1.provcodemoi,t1.distcodemoi,t1.subdistcodemoi,t1.mumoi) as areamoi
+
 ,concat(v.fname,' ',v.lname) as volanteer
-FROM 
+FROM
+-- houseowner 
 (
 SELECT 
 SPLIT_STR(if(substr(h.hno,1,1)='0',SPLIT_STR(h.hno,'-',2),h.hno),'/',1) as x1
@@ -16,13 +20,12 @@ SPLIT_STR(if(substr(h.hno,1,1)='0',SPLIT_STR(h.hno,'-',2),h.hno),'/',1) as x1
 FROM house h
 LEFT   JOIN person p
 ON h.pcucode=p.pcucodeperson AND h.pid=p.pid
-
 ) as t0
 LEFT JOIN person t1
 ON t0.pcucode=t1.pcucodeperson AND t0.hcode=t1.hcode
 LEFT JOIN person v
 ON t0.pcucodepersonvola=v.pcucodeperson AND t0.pidvola=v.pid 
-WHERE substr(t0.villcode,1,8)='84120702'
+WHERE substr(t0.villcode,1,8)='84120701'
 AND t1.dischargetype='9'
 -- GROUP BY t1.mumoi,t0.hnomoi
 GROUP BY t0.villcode,t0.hno
