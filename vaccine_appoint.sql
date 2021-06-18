5f3dd496a1a6da0a90a24d748a1c97b9d165b135
cantei <gcantei@gmail.com>
2021-06-18 23:32:56 +0700

    Update vaccine_appoint.sql

--- vaccine_appoint.sql
+++ vaccine_appoint.sql
@@ -1,12 +1,15 @@
-SELECT p.prename,p.fname,p.lname,p.birth,TIMESTAMPDIFF(MONTH,p.birth,CURDATE()) as  'agemonth'
+# ตรวจสอบวันนัด 
+SELECT p.idcard,CONCAT(c.titlename,p.fname,'  ',p.lname) as fullname,p.sex,p.birth,TIMESTAMPDIFF(MONTH,p.birth,CURDATE()) as  'agemonth'
 ,e.dateepi,GROUP_CONCAT(DISTINCT e.vaccinecode) as vaccines
-,GROUP_CONCAT(DISTINCT CONVERT( a.dateappoint, CHAR(255))) as appoint
-,GROUP_CONCAT(a.vaccinecode)  as need_vaccine
+,GROUP_CONCAT(DISTINCT CONVERT( a.dateappoint, CHAR(255)) ORDER BY a.dateappoint ASC) as appoint
+,GROUP_CONCAT(DISTINCT a.vaccinecode ORDER BY a.dateappoint ASC)  as need_vaccine
 FROM visitepi e
 INNER JOIN visitepiappoint a 
 ON e.pcucodeperson=a.pcucodeperson AND e.pid=a.pid 
 INNER JOIN person p
 ON e.pcucodeperson=p.pcucodeperson AND e.pid=p.pid 
-WHERE e.dateepi BETWEEN '2021-01-01' AND '2021-06-31'
+INNER JOIN ctitle c
+ON p.prename=c.titlecode
+WHERE e.dateepi BETWEEN '2018-01-01' AND '2021-06-31'
 AND a.dateappoint > e.dateepi
 GROUP BY e.pid ;
